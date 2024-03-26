867fcc82f8c72ed8ed70d7bddcec1c2f3e9e0522
ahmedgyar <ahmedyassergyar@gmail.com>
2024-03-26 23:25:50 +0200

    2024-03-26 11:25:50 PM

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -11,8 +11,11 @@
  * Return: void
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
-{
-    if (tree != NULL && func != NULL)
+{   
+    if(tree == NULL || func == NULL)
+        return 0;
+
+    if(tree != NULL && func != NULL)
     {
         binary_tree_postorder(tree->left, func);
         binary_tree_postorder(tree->right, func);
