0551333d71b7f2461fd58a01cb1a0c3a50666a0c
nadazkai21 <nadazaki019@gmail.com>
2024-03-26 23:32:36 +0200

    8

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -14,7 +14,7 @@
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
-		return (0);
+		return ;
 
 	if (tree != NULL && func != NULL)
 	{
