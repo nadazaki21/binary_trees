5caef50d62079404e6fee681c01dcd12c5808dab
nadazkai21 <nadazaki019@gmail.com>
2024-03-27 02:50:08 +0200

    trying 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,7 +1,9 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_balance - 
+ * binary_tree_balance -  function that measures the balance factor of a binary tree
+ * @tree: tree whose balance factor will be measured
+ * Return: the balance factor
 */
 int binary_tree_balance(const binary_tree_t *tree)
 {
