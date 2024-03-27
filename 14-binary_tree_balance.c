6629068305cb68305abf4c4db85f74ff76c16cf9
nadazkai21 <nadazaki019@gmail.com>
2024-03-27 17:48:25 +0200

    14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,68 +1,69 @@
 #include "binary_trees.h"
 
 /**
+ * height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree to measure the height
+ *
+ * Description: If tree is NULL, the height is 0
+ * Return: Height of the binary tree
+ */
+size_t height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		size_t left_height = height(tree->left);
+		size_t right_height = height(tree->right);
+
+		if (left_height > right_height)
+			return (left_height + 1);
+		else if (tree->left == NULL && tree->right == NULL)
+			return (0);
+		else
+			return (right_height + 1);
+	}
+}
+
+/**
  * binary_tree_balance -  function that measures the balance factor of a binary tree
  * @tree: tree whose balance factor will be measured
  * Return: the balance factor
 */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    /*int balance_factor;*/
-    size_t left_height = 0, right_height = 0;
+	int balance_factor;
+	size_t left_height = 0, right_height = 0;
 
-    if (tree == NULL)
-        return (0);
+	/* 
+	implementatoin with height function
+	*/
+	
+	if (tree->left != NULL)
+	{
+		left_height = height(tree->left);
+		left_height++;
+	}
+	else
+	{
+		left_height = 0;
+	}
+	
+	if (tree->right != NULL)
+	{
+		right_height = height(tree->right);
 
-    /*
-    if (tree->left != NULL)
-    {
-        left_height++;
-        left_height += binary_tree_balance(tree->left);
-    }
+		right_height++;
+	}
+	else
+	{
+		right_height = 0;
+	}
 
-    if (tree->right != NULL)
-    {
-        right_height++;
-        right_height += binary_tree_balance(tree->right);
-    }
-    
+	balance_factor = left_height - right_height;
 
-
-    printf("height left at node %d is %ld\n", tree->n, left_height);
-    printf("height right at node %d is %ld\n", tree->n, right_height);
-    */
-    /* balance_factor = left_height - right_height; */
-
-    return (0);
-
-    
-    /* 
-    implementatoin with height function
-    */
-    /*
-    if (tree->left != NULL)
-    {
-        left_height = binary_tree_height(tree->left);
-        left_height++;
-    }
-    else
-    {
-        left_height = 0;
-    }
-    
-    if (tree->right != NULL)
-    {
-        right_height = binary_tree_height(tree->right);
-
-        right_height++;
-    }
-    else
-    {
-        right_height = 0;
-    }
-
-    balance_factor = left_height - right_height;
-
-    return (balance_factor);
-    */
+	return (balance_factor);
+	
 }
